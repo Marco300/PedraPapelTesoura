47dff383ae70c4f61535f9ed5403dfb6383c3ea3
marco <bill_hcc@hotmail.com>
2019-04-16 18:57:25 -0300

    olaaa

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/*              VARIAVEIS                                */
+
 let userScore = 0;
 let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
@@ -9,6 +11,11 @@ const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 const result_user = document.querySelector(".resultUser > img")
 const result_comp = document.querySelector(".resultComp > img")
+let result_message = document.getElementById("result-message")
+
+
+
+/*              ESCOLHA DO COMPUTADOR                                */
 
 function getComputerChoice(){
     const choices = ['r', 'p', 's'];
@@ -16,6 +23,7 @@ function getComputerChoice(){
     return choices[randomNumber];    
 }
 
+/* C   ONVERTE A LETRA DA ESCOLHA PARA O NOME DO MOVIMENTO          */
 function convertToWord(letter){
         
     if(letter === "r") return "Pedra"; else
@@ -23,46 +31,50 @@ function convertToWord(letter){
     return "Tesoura";
 } 
 
+/*              GANHAR / PERDER / EMPATAR                  */
+
 function win(userChoice, computerChoice){
     userScore++; 
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-
-    const smallUserWord = "user".fontsize(3).sub().fontcolor('red');
-    const smallCompWord = "comp".fontsize(3).sub().fontcolor('red');
-   //result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord}   vence   ${convertToWord(computerChoice)}${smallCompWord}. vc Ganhou!!!`;
-    
+    setUserImg(userChoice);
+    setCompImg(computerChoice); 
+    result_message.innerHTML = `${convertToWord(userChoice)}   vence   ${convertToWord(computerChoice)}. vc Ganhou!!!`;
 }
+
+
 function lose(userChoice, computerChoice){
     computerScore++; 
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    const smallUserWord = "user".fontsize(3).sub();
-    const smallCompWord = "comp".fontsize(3).sub();
-    //result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} perde para ${convertToWord(computerChoice)}${smallCompWord}. vc Perdeu!!!`;   
-    
+    setUserImg(userChoice);               
+    setCompImg(computerChoice);   
+    result_message.innerHTML = `${convertToWord(userChoice)} perde para ${convertToWord(computerChoice)}. vc Perdeu!!!`;   
 }
-function draw(userChoice, computerChoice){
-    
+
+function draw(userChoice, computerChoice){    
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    const smallUserWord = "user".fontsize(3).sub();
-    const smallCompWord = "comp".fontsize(3).sub();
-    //result_p.innerHTML = "EMPATE !!!!"
+    setUserImg(userChoice);                
+    setCompImg(computerChoice);   
+    result_message.innerHTML = "EMPATE !!!!"
 }
+
+/* SETA AS IMAGENS DE A CORDO COM A ESCOLHA DO USER OU DO CPOMPUTER */
 
 function setUserImg(user){
     if(user === "r") result_user.src = 'images/rock.png';else
     if(user === "p") result_user.src = 'images/paper.png'; else
-    result_user.src = 'images/scissors.png'; 
+    if(user === "s") result_user.src = 'images/scissors.png';  
 }
 
 function setCompImg(comp){
     if(comp === "r") result_comp.src = 'images/rock.png';else
     if(comp === "p") result_comp.src = 'images/paper.png'; else
-    result_user.src = 'images/scissors.png'; 
+    if(comp === "s") result_comp.src = 'images/scissors.png'; 
 }
 
+/*                    REGRAS DO JOGO                              */
 function game(userChoice) {
     const computerChoice = getComputerChoice();
     switch(userChoice + computerChoice){
@@ -70,22 +82,19 @@ function game(userChoice) {
         case "pr":
         case "sp":
             win(userChoice,computerChoice);
-            setUserImg(userChoice);
-            setCompImg(computerChoice);
+            
             break;
         case "rp":
         case "ps":
         case "sr":
             lose(userChoice,computerChoice);
-            setUserImg(userChoice);
-            setCompImg(computerChoice);
+            
             break;
         case "rr":
         case "pp":
         case "ss":
             draw(userChoice,computerChoice);
-            setUserImg(userChoice);
-            setCompImg(computerChoice);
+            
             break;
     }
 }
@@ -98,6 +107,7 @@ function main() {
 
     paper_div.addEventListener('click', function(){
         game("p");
+        
     })
 
     scissors_div.addEventListener('click', function(){
